88b34567352672937b010e38837749aad2936df6
Void <121253583+Void-1999@users.noreply.github.com>
2024-07-07 15:05:56 +0530

    Create theme.config.js

---
+++ theme.config.js
@@ -0,0 +1,10 @@
+export default {
+  logo: 'Xiao Marie Docs',
+  project: {
+    link: 'https://github.com/Void-1999/Xiao-Marie-Docs' 
+  },
+  docsRepositoryBase: 'https://github.com/Void-1999/Xiao-Marie-Docs/blob/main/pages',
+  footer: {
+    text: 'Xiao Marie © 2023'
+  }
+};
